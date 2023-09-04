71c493fb07bf48b676b26c3699f4a9e5c7e7a4c6
heorh11 <gheorghe.filip225@gmail.com>
2023-09-04 20:36:13 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
-galleryContainer.innerHTML = GalleryMarkup;
-const GalleryMarkup = galleryItems.map(galleryItem => `
-    <li class='gallry__item'>
-     <a class='gallery__link' href='${item.original}'>
-      <img class='gallery__image'
-      src='${item.preview}'>
-      alt='${item.description}'
-      data-source='${item.original}'>
-      </img>
-     </li>
-    </a>
-`).join('')
 
+const GalleryMarkup = galleryItems.map(item => `
+  <li class='gallry__item'>
+    <a class='gallery__link' href='${item.original}'>
+      <img class='gallery__image' src='${item.preview}' alt='${item.description}' data-source='${item.original}'>
+    </a>
+  </li>
+`).join('');
+
+galleryContainer.innerHTML = GalleryMarkup;
 
 galleryContainer.addEventListener('click', evt => {
-    evt.preventDefault();
-    if (evt.target.tagName === 'IMG') {
-        const largeImgSrc = evt.target.getAttribute('data-source')
-    }
-} )
+  evt.preventDefault();
+  if (evt.target.tagName === 'IMG') {
+    const largeImgSrc = evt.target.getAttribute('data-source');
+    const instance = basicLightbox.create(`
+      <div>
+        <img src="${largeImgSrc}" width="800" height="600">
+      </div>
+    `);
+    instance.show();
+  }
+});
 
-
-
-const instance = basicLightbox.create(`
-<div>
-    <img src="assets/images/image.png" width="800" height="600">
-    </div>
-`)
-
-instance.show()
 console.log(galleryItems);
+console.log(galleryItems);
+
+
+
+
